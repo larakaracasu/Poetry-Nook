8e2f70dc2eda9e413bdf3f6fc80ce6b7f0b0422a
Lara Karacasu <larakaracasu@gmail.com>
2023-07-27 01:32:32 -0400

    Alphabetized poems.

--- scripts.js
+++ scripts.js
@@ -1,28 +1,54 @@
 window.onload = function() {
     fetch('poems.txt')
         .then(response => response.text())
-        .then(data => populatePoems(data));
+        .then(data => {
+            // Parse the poems from the fetched data
+            const parsedPoems = parsePoems(data);
+            // Sort the poems
+            const sortedPoems = sortPoems(parsedPoems);
+            // Display the poems
+            populatePoems(sortedPoems);
+        });
 }
 
-function populatePoems(data) {
+function parsePoems(data) {
+    var poemStrs = data.split("\r\n\r\n*").map(poem => poem.trim());  // Split using "\r\n\r\n*"
+
+    // Convert each poem string into an object with separate title and lines properties
+    var poems = poemStrs.map(function(poemStr) {
+        var poemLines = poemStr.split("\r\n");
+        var title = poemLines.shift();
+        title = title.replace('*', ''); // remove the asterisk from the title
+        return { title: title.trim(), lines: poemLines }; // use trim() to remove any leading/trailing spaces from the title
+    });
+
+    return poems;
+}
+
+
+
+function sortPoems(poems) {
+    console.log('Before sorting::', poems.map(poem => poem.title));  // log titles before sorting
+    var sortedPoems = poems.sort((a, b) => a.title.localeCompare(b.title, 'en', { sensitivity: 'base' }));
+    console.log('After sorting::', sortedPoems.map(poem => poem.title));  // log titles after sorting
+    return sortedPoems;
+}
+
+
+function populatePoems(poems) {
     var container = document.getElementById('poemContainer');
-    var poems = data.split("*"); 
 
-    poems.forEach(function(poemStr) {
-        poemStr = poemStr.trim(); // remove leading and trailing whitespace
-        var poemLines = poemStr.split("\n");
-        var titleText = poemLines.shift();
-
+    poems.forEach(function(poem) {
         var gridItem = document.createElement('div');
         gridItem.className = 'grid-item';
 
         var title = document.createElement('h2');
-        title.innerText = titleText;
+        title.innerText = poem.title;
         gridItem.appendChild(title);
 
         var poemText = document.createElement('p');
         poemText.className = 'poem';
-        poemText.innerText = poemLines.join('\n');
+        poemText.innerText = poem.lines.join('\n');
         gridItem.appendChild(poemText);
 
         container.appendChild(gridItem);
